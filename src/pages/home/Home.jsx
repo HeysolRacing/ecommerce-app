c50f14875e92089914b000de2fc520425e672653
Heysel Sagaon <heysel.sagaon@gmail.com>
2023-10-09 21:47:23 -0600

    All components to display all products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
-import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Track from '../../components/track/Track'
+import Testimonial from '../../components/testimonial/Testimonial';
 
 function Home() {
   return (
     <Layout>
-      <h3>Home</h3>
+      <HeroSection />
+      <Filter />
+      <ProductCard/>
+      <Track/>
+      <Testimonial />
     </Layout>
   )
 }
