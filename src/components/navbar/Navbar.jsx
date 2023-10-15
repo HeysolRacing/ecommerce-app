7b9ec52cb6b4ff7dead2fa87e647a6df3fcbc318
Heysel Sagaon <heysel.sagaon@gmail.com>
2023-10-15 00:37:18 -0600

    CRUD Product on Cart

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,24 @@
 import { Fragment, useContext, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { Link } from 'react-router-dom'
+import myContext from '../../context/data/myContext'
 import { BsFillCloudSunFill } from 'react-icons/bs'
 import { FiSun } from 'react-icons/fi'
-import myContext from '../../context/data/myContext'
+import { Link } from 'react-router-dom'
+import { Dialog, Transition } from '@headlessui/react'
 import { RxCross2 } from 'react-icons/rx'
 import { useSelector } from 'react-redux'
 
 function Navbar() {
-  const [open, setOpen] = useState(false)
-
   const context = useContext(myContext)
   const { toggleMode, mode } = context
 
+  const [open, setOpen] = useState(false)
+
   const user = JSON.parse(localStorage.getItem('currentUser'))
-  const admin = JSON.parse(localStorage.getItem('currentAdmin'))
+  //const admin = JSON.parse(localStorage.getItem('currentAdmin'))
 
   const logout = () => {
-    localStorage.clear('currentUser')
-    localStorage.clear('currentAdmin');
-    window.location.href = "/"
+    localStorage.clear('user')
+    window.location.href = "/login"
   }
 
   const cartItems = useSelector((state) => state.cart)
@@ -202,7 +201,7 @@ function Navbar() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                     </svg>
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>0</span>
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>{cartItems.length}</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </Link>
                 </div>
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit' 
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 
 export const store = configureStore({
-  reducer: {
-    
-  },
-  devTools:true
+    reducer :{
+        cart : cartSlice
+    },
+    devTools : true
 })
