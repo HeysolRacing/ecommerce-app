7a4f96a1d37e22833c0310637e782700e68cf4de
Heysel Sagaon <heysel.sagaon@gmail.com>
2023-10-15 16:50:32 -0600

    Firebase template

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQkCC0McMsglr9FOM_G42iIHLbKGOUl7k",
+  apiKey: "AKI KEY HERE",
   authDomain: "ecommerce-c7ec1.firebaseapp.com",
   projectId: "ecommerce-c7ec1",
   storageBucket: "ecommerce-c7ec1.appspot.com",
-  messagingSenderId: "666318416561",
-  appId: "1:666318416561:web:a279746a250ea29dc15090"
+  messagingSenderId: "XXXXXXXXXXXXXX",
+  appId: "API ID HERE XXXXXXXXXX"
 };
 
 // Initialize Firebase
