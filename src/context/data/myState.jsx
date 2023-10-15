744fc4299495c2612e125e867ab24b60e2635c44
Heysel Sagaon <heysel.sagaon@gmail.com>
2023-10-14 23:19:08 -0600

    Update and Delete functionality

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import MyContext from './myContext';
-import { fireDb } from '../../firebase/FirebaseConfig'
-import { Timestamp, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import MyContext from "./myContext";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { fireDb } from "../../firebase/FirebaseConfig";
 
 function MyState(props) {
-  const [mode, setMode] = useState('light');  
-  const [loading, setLoading] = useState(false); 
+  const [mode, setMode] = useState("light");
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = 'rgb(17, 24, 39)';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17, 24, 39)";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-  }
+  };
+
+  const [loading, setLoading] = useState(false);
 
   const [products, setProducts] = useState({
     title: null,
@@ -26,108 +37,127 @@ function MyState(props) {
     category: null,
     description: null,
     time: Timestamp.now(),
-    date: new Date().toLocaleString(
-      "en-US",
-      {
-        month: "short",
-        day: "2-digit",
-        year: "numeric",
-      }
-    )
-  })
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
 
   // ********************** Add Product Section  **********************
   const addProduct = async () => {
-    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
-      return toast.error('Please fill all fields')
+    if (
+      products.title == null ||
+      products.price == null ||
+      products.imageUrl == null ||
+      products.category == null ||
+      products.description == null
+    ) {
+      return toast.error("all fields are required");
     }
-    setLoading(true)
+
+    setLoading(true);
+
     try {
-      const productRef = collection(fireDb, "products")
-      await addDoc(productRef, products)
-      toast.success("Add Product successfully")
+      const productRef = collection(fireDb, "products");
+      await addDoc(productRef, products);
+      toast.success("Add product successfully");
       setTimeout(() => {
-        window.location.href = '/dashboard'
+        window.location.href = "/dashboard";
       }, 800);
-      getProductData()
-      setLoading(false)
+      getProductData();
+      setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-    setProducts("")
-  }
+    // setProducts("")
+  };
 
   const [product, setProduct] = useState([]);
 
   // ****** get product
   const getProductData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const q = query(
         collection(fireDb, "products"),
-        orderBy("time"),
-        // limit(5)
+        orderBy("time")
       );
       const data = onSnapshot(q, (QuerySnapshot) => {
         let productsArray = [];
         QuerySnapshot.forEach((doc) => {
           productsArray.push({ ...doc.data(), id: doc.id });
         });
-        setProduct(productsArray)
+        setProduct(productsArray);
         setLoading(false);
       });
+
       return () => data;
+    
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getProductData();
   }, []);
 
   const edithandle = (item) => {
-    setProducts(item)
-  }
-
+    setProducts(item);
+  };
+  // update product
   const updateProduct = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-        await setDoc(doc(fireDb, 'products', products.id), products)
-        toast.success("Product Updated successfully")
-        setTimeout(() => {
-            window.location.href = '/dashboard'
-        }, 800);
-        getProductData();
-        setLoading(false)
+      await setDoc(doc(fireDb, "products", products.id), products);
+      toast.success("Product Updated successfully");
+      setTimeout(() => {
+        window.location.href = "/dashboard";
+      }, 800);
+      getProductData();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
-    } catch (error) {
-        console.log(error)
-        setLoading(false)
-    }
-  }
-// delete product
+  // delete product
   const deleteProduct = async (item) => {
-    setLoading(true)
+    setLoading(true);
     try {
-        await deleteDoc(doc(fireDb, 'products', item.id))
-        toast.success('Product Deleted successfully')
-        getProductData();
-        setLoading(false)
+      await deleteDoc(doc(fireDb, "products", item.id));
+      toast.success("Product Deleted successfully");
+      getProductData();
+      setLoading(false);
     } catch (error) {
-        console.log(error)
-        setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  }
+  };
 
   return (
-    <MyContext.Provider value={{ mode, toggleMode, loading, setLoading,
-      products, setProducts,addProduct, product }}>
+    <MyContext.Provider
+      value={{
+        mode,
+        toggleMode,
+        loading,
+        setLoading,
+        products,
+        setProducts,
+        addProduct,
+        product,
+        edithandle, 
+        updateProduct, 
+        deleteProduct
+      }}
+    >
       {props.children}
     </MyContext.Provider>
-  )
+  );
 }
 
-export default MyState
+export default MyState;
